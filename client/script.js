a52ba517ad1b2258cb6e23c3fb79762b6f5e1a95
Sam Hardiment <samuelghardiment@gmail.com>
2022-05-16 11:05:45 +0100

    HTML changes

--- client/script.js
+++ client/script.js
@@ -2,6 +2,7 @@
 // const subName = require("../data/random_name");
 
 const API_Key = "yMYTtCg4jPmk6BxD19dklT7FUUfAMQAD";
+
 const titleName = ["Lord", "The Common", "King", "The Divine"];
 const subName = ["Apple", "Fish", "Salt", "Pepper"];
 
@@ -11,6 +12,11 @@ function getRandomInt(min, max) {
 function randomNameGenerator() {
   console.log(titleName[getRandomInt(0, titleName.length)]);
   console.log(subName[getRandomInt(0, subName.length)]);
+  let name = `${titleName[getRandomInt(0, titleName.length)]} ${
+    subName[getRandomInt(0, subName.length)]
+  }`;
+  console.log(name);
+  return name;
 }
 
 randomNameGenerator();
