b240d383df44959cf04ba2b25b3588c65b0d6513
Sam Hardiment <samuelghardiment@gmail.com>
2022-05-16 10:40:39 +0100

    randomName Func

--- client/script.js
+++ client/script.js
@@ -1,13 +1,16 @@
-const titleName = require("../data/random_name");
-const subName = require("../data/random_name");
+// const titleName = require("../data/random_name");
+// const subName = require("../data/random_name");
 
 const API_Key = "yMYTtCg4jPmk6BxD19dklT7FUUfAMQAD";
+const titleName = ["Lord", "The Common", "King", "The Divine"];
+const subName = ["Apple", "Fish", "Salt", "Pepper"];
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-function randomeNameGenerator() {
-  console.log(titleName[getRandomInt(0, titleName.length + 1)]);
+function randomNameGenerator() {
+  console.log(titleName[getRandomInt(0, titleName.length)]);
+  console.log(subName[getRandomInt(0, subName.length)]);
 }
 
-randomeNameGenerator();
+randomNameGenerator();
--- data/random_name.js
+++ data/random_name.js
@@ -1,4 +1,4 @@
-titleName = ["Lord", "The Common", "King", "The Divine"];
-subName = ["Apple", "Fish", "Salt", "Pepper"];
+const titleName = ["Lord", "The Common", "King", "The Divine"];
+const subName = ["Apple", "Fish", "Salt", "Pepper"];
 
 module.exports = { titleName, subName };
