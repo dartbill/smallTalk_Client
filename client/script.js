5876719ca669300402fd9e7fac40d582a5a03070
Sam Hardiment <samuelghardiment@gmail.com>
2022-05-16 10:31:48 +0100

    add data array + random name func

--- client/script.js
+++ client/script.js
@@ -1,1 +1,13 @@
+const titleName = require("../data/random_name");
+const subName = require("../data/random_name");
+
 const API_Key = "yMYTtCg4jPmk6BxD19dklT7FUUfAMQAD";
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+function randomeNameGenerator() {
+  console.log(titleName[getRandomInt(0, titleName.length + 1)]);
+}
+
+randomeNameGenerator();
---
+++ data/random_name.js
@@ -0,0 +1,4 @@
+titleName = ["Lord", "The Common", "King", "The Divine"];
+subName = ["Apple", "Fish", "Salt", "Pepper"];
+
+module.exports = { titleName, subName };
